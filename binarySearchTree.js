89bd158ddbf245d02bf27d62822165f7cc7194b6
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-08-03 18:06:07 -0300

    create tree list

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -3,8 +3,15 @@ class Tree {
     this.root = this.buildTree([... new Set(array)].sort(( a, b ) => { return a - b }));
   }
 
-  buildTree(array) {
-    return array;
+  buildTree(array, start = 0, end = array.length - 1) {
+    if (start > end) return null;
+    const middle = Math.floor((start + end) / 2);
+    const root = new Node(array[middle]);
+
+    root.left = this.buildTree(array, start, middle - 1);
+    root.right = this.buildTree(array, middle + 1, end);
+
+    return root;
   }
 }
 
@@ -20,4 +27,4 @@ class Node {
 // testing
 const list = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const treeList = new Tree(list);
-console.log(treeList);
+console.log(JSON.stringify(treeList, null, 4));

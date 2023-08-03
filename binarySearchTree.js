850911356461dc168207d8693fe4d6487d36ab63
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-08-03 18:11:15 -0300

    add prettyPrint function for tree visualization
    This function is from The Odin Project, not my own code, for visualizing
    the binary tree in the console. NodeJs is required for this.

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -27,4 +27,19 @@ class Node {
 // testing
 const list = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const treeList = new Tree(list);
-console.log(JSON.stringify(treeList, null, 4));
+// console.log(JSON.stringify(treeList, null, 4));
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+prettyPrint(treeList.root);
